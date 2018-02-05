15e7b668cba831f0f96bf33343c759875a31a1e9
jaamito <gm.2012.ian.lopez@gmail.com>
2018-02-05 17:55:52 +0100

    Registro

--- foro/resources/views/auth/register.blade.php
+++ foro/resources/views/auth/register.blade.php
@@ -25,6 +25,74 @@
                             </div>
                         </div>
 
+                        <!--Direccion-->
+                        <div class="form-group{{ $errors->has('direccion') ? ' has-error' : '' }}">
+                            <label for="direccion" class="col-md-4 control-label">Direccion</label>
+
+                            <div class="col-md-6">
+                                <input id="direccion" type="text" class="form-control" name="direccion" value="{{ old('direccion') }}" required autofocus>
+
+                                @if ($errors->has('direccion'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('direccion') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!--/Direccion-->
+
+                        <!--Edad-->
+                        <div class="form-group{{ $errors->has('edad') ? ' has-error' : '' }}">
+                            <label for="edad" class="col-md-4 control-label">Edad</label>
+
+                            <div class="col-md-6">
+                                <input id="edad" type="text" class="form-control" name="edad" value="{{ old('edad') }}" required autofocus>
+
+                                @if ($errors->has('edad'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('edad') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!--/Edad-->
+
+                        <!--TLF-->
+                        <div class="form-group{{ $errors->has('tlf') ? ' has-error' : '' }}">
+                            <label for="tlf" class="col-md-4 control-label">TLF</label>
+
+                            <div class="col-md-6">
+                                <input id="tlf" type="text" class="form-control" name="tlf" value="{{ old('tlf') }}" required autofocus>
+
+                                @if ($errors->has('tlf'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('tlf') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!--/TLF-->
+                        <!--Admin-->
+                        <div class="form-group{{ $errors->has('admin') ? ' has-error' : '' }}">
+                            <label for="admin" class="col-md-4 control-label">Admin</label>
+
+                            <div class="col-md-6">
+                                <label for="admin" class="col-md-4 control-label"><input id="admin" type="radio" name="admin" value="1" >Si</label>
+                                
+                                <label for="admin" class="col-md-4 control-label"><input id="admin" type="radio"  name="admin" value="0" >No</label>
+                                
+
+
+                                @if ($errors->has('admin'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('admin') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!--/Admin-->
+
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
