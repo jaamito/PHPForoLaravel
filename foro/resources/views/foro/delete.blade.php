e22b9ffd8302812c845838f41142f76d4dabbe86
jaamito <gm.2012.ian.lopez@gmail.com>
2018-02-14 18:45:17 +0100

    delete

--- foro/resources/views/foro/delete.blade.php
+++ foro/resources/views/foro/delete.blade.php
@@ -1,9 +1,7 @@
-@extends('layouts.app')
-@section('content')
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
 <div class="alert alert-success">
   <strong>¡Bien hecho!</strong> Eliminado con exito.
 </div>
 <ul class="nav nav-pills">
   <li class="active"><a href="{{ url('inicio') }}">Inicio</a></li>
 </ul>
-@endsection
