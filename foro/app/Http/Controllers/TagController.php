5b75a3578b335695f48654403f6a54fbe2f0b75e
erdeks <aponcelasgarcia@iesesteveterradas.cat>
2018-02-19 16:54:09 +0100

    rutas agregadas

--- foro/app/Http/Controllers/TagController.php
+++ foro/app/Http/Controllers/TagController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Tag;
 use App\User;
+use App\Post;
 class TagController extends Controller
 {
   public function crear()
@@ -16,7 +17,8 @@ class TagController extends Controller
       $tag->idUsuario = $request->input('idUsuario');
       $tag->nombre = $request->input('nombre');
       $tag->save();
-      return view('foro.bien');
+      $arrayTags = Tag::select()->orderBy('id', 'desc')->get();
+      return view('foro.verTags', array('arrayTags'=>$arrayTags));
   }
 
 
@@ -26,7 +28,8 @@ class TagController extends Controller
         $arrayTags = Tag::select()->where('nombre','LIKE','%'.$buscar.'%')->get();
         $contTag = Tag::all();
         $contuser = User::all();
-        return view('foro.verTags', array('arrayTags'=>$arrayTags, '$cont1'=>$contTag,'$cont2'=>$contuser));
+        $arrayPost = Post::select()->orderBy('id', 'desc')->get();
+        return view('foro.verTags', array('arrayTags'=>$arrayTags, '$cont1'=>$contTag,'$cont2'=>$contuser, ));
     }
 
   public function delete($id) {
